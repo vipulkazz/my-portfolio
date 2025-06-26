8835980ef5b703feccc6f5528ba7f4da6f91e2db
vipul kaushik <vipulkaushik@vipuls-MacBook-Pro.local>
2025-06-26 11:06:40 +0530

    added site map and meta tags

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -31,6 +31,23 @@ const Layout = ({ children }) => {
         />
         <meta name="author" content="Vipul kaushik" />
         <meta name="theme-color" content="#f13024" />
+        
+        {/* Open Graph meta tags for rich previews */}
+        <meta property="og:title" content="Vipul Kaushik" />
+        <meta property="og:description" content="Entrepreneur, technologist, and builder. Passionate about solving real-world problems with engineering and design." />
+        <meta property="og:url" content="https://www.vipulkaushik.com" />
+        <meta property="og:image" content="https://www.vipulkaushik.com/SubjectOne.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Vipul Kaushik" />
+        
+        {/* Additional meta tags for better social sharing */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Vipul Kaushik" />
+        <meta name="twitter:description" content="Entrepreneur, technologist, and builder. Passionate about solving real-world problems with engineering and design." />
+        <meta name="twitter:image" content="https://www.vipulkaushik.com/SubjectOne.png" />
+        
+        {/* Sitemap */}
+        <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" />
       </Head>
 
       <TopLeftImg />
